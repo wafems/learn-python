bd80af6bb4413834150a4ca01df6659639e921d0
wafems <1243453218@qq.com>
2017-07-22 03:44:06 +0800

    add another way

--- download_cat.py
+++ download_cat.py
@@ -1,6 +1,6 @@
 import urllib.request
 
-response = urllib.request.urlopen("http://placekitten.com/g/500/700")
+# response = urllib.request.urlopen(req)
 cat_img = response.read()
 
 with open("cat_500_600.jpg","wb")as f:
